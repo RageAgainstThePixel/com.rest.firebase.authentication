6d8a5906499169931555dd14a542d86c492aad88
Stephen Hodgson <hodgson.designs@gmail.com>
2021-06-17 14:56:09 -0400

    added preserve assembly attribute so that json types don't get stripped for AOT builds

--- Firebase.Authentication/Packages/com.firebase.authentication/Runtime/AssemblyInfo.cs
+++ Firebase.Authentication/Packages/com.firebase.authentication/Runtime/AssemblyInfo.cs
@@ -1,7 +1,10 @@
 ﻿// Licensed under the MIT License. See LICENSE in the project root for license information.
 
 using System.Runtime.CompilerServices;
+using UnityEngine.Scripting;
 
+[assembly: Preserve]
+[assembly: AlwaysLinkAssembly]
 [assembly: InternalsVisibleTo("Firebase.Authentication.Tests")]
 [assembly: InternalsVisibleTo("Firebase.Authentication.Editor")]
 [assembly: InternalsVisibleTo("Firebase.Storage")]
