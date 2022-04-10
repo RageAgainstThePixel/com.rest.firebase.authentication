e5fc367e493334fbe5eab243f4bdddbf39c1a745
Stephen Hodgson <hodgson.designs@gmail.com>
2022-04-10 15:14:09 -0400

    added a few useful extensions

---
+++ Runtime/StringExtensions.cs
@@ -0,0 +1,24 @@
+﻿// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Firebase.Authentication
+{
+    internal static class StringExtensions
+    {
+        /// <summary>
+        /// Generates a <see cref="Guid"/> based on the string.
+        /// </summary>
+        /// <param name="string">The string to generate the <see cref="Guid"/>.</param>
+        /// <returns>A new <see cref="Guid"/> that represents the string.</returns>
+        internal static Guid GenerateGuid(this string @string)
+        {
+            using (MD5 md5 = MD5.Create())
+            {
+                return new Guid(md5.ComputeHash(Encoding.Default.GetBytes(@string)));
+            }
+        }
+    }
+}
