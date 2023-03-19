88ecc050bcaa026133f302b5406fd51ede2caba7
Stephen Hodgson <hodgson.designs@gmail.com>
2023-03-19 00:36:29 -0400

    Refactored client setup
    Added the ability to specify custom user credential store
    update unit tests with async
    misc formatting

---
+++ Firebase.Authentication/Packages/com.rest.firebase.authentication/Runtime/CredentialStore/AbstractUserCredentialStore.cs
@@ -0,0 +1,26 @@
+﻿// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System.Threading.Tasks;
+
+namespace Firebase.Authentication.CredentialStore
+{
+    /// <summary>
+    /// Repository abstraction for <see cref="FirebaseUser"/>.
+    /// </summary>
+    public abstract class AbstractUserCredentialStore : IUserCredentialStore
+    {
+        internal FirebaseConfiguration Configuration { get; set; }
+
+        /// <inheritdoc />
+        public abstract bool UserExists { get; }
+
+        /// <inheritdoc />
+        public abstract FirebaseUser GetUser { get; protected set; }
+
+        /// <inheritdoc />
+        public abstract Task SaveUserAsync(FirebaseUser newUser);
+
+        /// <inheritdoc />
+        public abstract Task DeleteUserAsync();
+    }
+}
---
+++ Firebase.Authentication/Packages/com.rest.firebase.authentication/Runtime/CredentialStore/InMemoryCredentialStore.cs
@@ -0,0 +1,29 @@
+﻿// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System.Threading.Tasks;
+
+namespace Firebase.Authentication.CredentialStore
+{
+    /// <summary>
+    /// <see cref="IUserCredentialStore"/> implementation which saves user data in memory.
+    /// </summary>
+    /// <inheritdoc />
+    public sealed class InMemoryCredentialStore : AbstractUserCredentialStore
+    {
+        public override bool UserExists => GetUser != null;
+
+        public override FirebaseUser GetUser { get; protected set; }
+
+        public override async Task SaveUserAsync(FirebaseUser newUser)
+        {
+            GetUser = newUser;
+            await Task.CompletedTask;
+        }
+
+        public override async Task DeleteUserAsync()
+        {
+            GetUser = null;
+            await Task.CompletedTask;
+        }
+    }
+}
